e72502ecea49ad536cee4298bab72603fbe48d65
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-06-29 16:56:24 +0300

    Создано начальное окно для ввода поискового запроса о фильме.

--- app/src/main/java/com/imdb/film/kino/stars/animation/filmfilmfilm/view/activity/MainActivity.kt
+++ app/src/main/java/com/imdb/film/kino/stars/animation/filmfilmfilm/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.imdb.film.kino.stars.animation.filmfilmfilm
+package com.imdb.film.kino.stars.animation.filmfilmfilm.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.imdb.film.kino.stars.animation.filmfilmfilm.R
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
