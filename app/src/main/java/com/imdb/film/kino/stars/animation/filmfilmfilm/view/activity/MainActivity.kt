20c72f8607278079122906910deea25f69db74cf
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-06-30 16:58:58 +0300

    Merge pull request #2
    * Создано начальное окно для ввода поискового запроса о фильме.
    
    * Создан макет окна с результатами поиска, с использованием пагинации.
    
    * Создан макет для детальной информации о выбранном фильме.
    
    * Создан макет для элемента RecyclerView с данными об актерах.

---
+++ app/src/main/java/com/imdb/film/kino/stars/animation/filmfilmfilm/view/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.imdb.film.kino.stars.animation.filmfilmfilm.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.imdb.film.kino.stars.animation.filmfilmfilm.R
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.fragment_result_pages)
+        setContentView(R.layout.fragment_result_film)
+    }
+}
